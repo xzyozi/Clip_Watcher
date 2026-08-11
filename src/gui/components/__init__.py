d73657c78918932c9116a0564148865ef36273d6
xzyozi <xzyoi10sopmon@gmail.com>
2026-08-12 01:41:46 +0900

    fix(ui): remove obsolete imports in components __init__.py

--- src/gui/components/__init__.py
+++ src/gui/components/__init__.py
@@ -3,11 +3,7 @@ This package contains various GUI components used in the Clip Watcher applicatio
 """
 
 from .history_list_component import HistoryListComponent
-from .phrase_edit_component import PhraseEditComponent
-from .phrase_list_component import PhraseListComponent
 
 __all__ = [
     "HistoryListComponent",
-    "PhraseEditComponent",
-    "PhraseListComponent",
 ]
